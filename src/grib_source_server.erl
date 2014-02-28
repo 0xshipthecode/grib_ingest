912db4480932fcd0caf8e06e55aebe3b6862b476
Martin Vejmelka <vejmelkam@gmail.com>
2014-02-27 18:08:13 -0700

    added grib_source_info() to grib_source_server.erl

--- src/grib_source_server.erl
+++ src/grib_source_server.erl
@@ -9,7 +9,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/3,retrieve_gribs/5]).
+-export([start_link/3,retrieve_gribs/5,grib_source_info/1]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -22,6 +22,7 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
+
 start_link(GribSrc,StorDir,LogF) ->
     gen_server:start_link(?MODULE, [GribSrc,StorDir,LogF], []).
 
@@ -29,13 +30,16 @@ start_link(GribSrc,StorDir,LogF) ->
 retrieve_gribs(Pid,From,To,AtTime,Delta) ->
   gen_server:call(Pid,{retrieve_gribs,From,To,AtTime,Delta},infinity).
 
+grib_source_info(Pid) ->
+  gen_server:call(Pid,get_grib_source).
+
 
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
 
 init(Args=[#grib_source{name=N},_,LogF]) ->
-  LogF(info,"grib_source_server [~p] is being initialized.", [N]),
+  LogF(info,"grib_source_server [~p] registering with grib_ingest_server.", [N]),
   grib_ingest_server:register_server(N,self()),
   process_flag(trap_exit,true),
   {ok, Args}.
@@ -45,6 +49,8 @@ handle_call(Request, _From, State=[GS=#grib_source{name=N},StorDir,LogF]) ->
   case Request of
     {retrieve_gribs,From,To,AtTime,Delta} ->
       {reply, retrieve_gribs(From,To,AtTime,Delta,StorDir,GS,LogF), State};
+    grib_source_info ->
+      {reply, GS, State};
     Other ->
       LogF(flash, "grib_server [~p] received an unexpected message ~p.", [N,Other]),
       {reply, invalid_request, State}
@@ -64,19 +70,24 @@ terminate(shutdown, [#grib_source{name=N},_,LogF]) ->
   grib_ingest_server:unregister_server(N),
   ok.
 
+
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
 
 %% ------------------------------------------------------------------
 %% Internal Functions
 %% ------------------------------------------------------------------
 
+
 -spec compute_manifest(calendar:datetime(),calendar:datetime(),calendar:datetime(),integer(),#grib_source{}) ->
-        unsatisfiable|{calendar:datetime(),calendar:datetime(),[string()]}.
+          unsatisfiable|{calendar:datetime(),calendar:datetime(),calendar:datetime(),[string()]}.
 compute_manifest(From,To,AtTime,Delta,#grib_source{cycles=Cs,delay=Dhrs,file_hours=Fhrs,name_fun=F}) ->
   ToAdjusted = cycle_logic:shift_by_hours(AtTime,-Dhrs),
-  LC0 = cycle_logic:latest_cycle_for_time(ToAdjusted,Cs),
-  LC = cycle_logic:shift_cycle(LC0,Delta,Cs),
+  LC0 = cycle_logic:latest_cycle_for_time(From,Cs),
+  LC1 = cycle_logic:latest_cycle_for_time(ToAdjusted,Cs),
+  LC2 = cycle_logic:cull_cycle(LC0,LC1),
+  LC = cycle_logic:shift_cycle(LC2,Delta,Cs),
   case cycle_logic:gribs_for_interval(From,To,LC,Fhrs) of
     unsatisfiable ->
       unsatisfiable;
@@ -85,7 +96,7 @@ compute_manifest(From,To,AtTime,Delta,#grib_source{cycles=Cs,delay=Dhrs,file_hou
       CovFrom = cycle_logic:shift_by_hours(LC,Fh0),
       {LC,Fh1} = lists:last(Rest),
       CovTo = cycle_logic:shift_by_hours(LC,Fh1),
-      {CovFrom,CovTo,GribNames}
+      {LC,CovFrom,CovTo,GribNames}
   end.
 
 
@@ -99,28 +110,59 @@ download_gribs(List,StorDir,#grib_source{name=N,url_prefix=Pfix,domain=D},LogF) 
       LogF(info, "grib_server [~p] -> downloading ~p to ~p.", [N,Id,AbsPath]),
       case httpc:request(get, {Url,[]}, [], [{stream, AbsPath}]) of
         {ok,saved_to_file} ->
-          LogF(info, "grib_server [~p] -> finished downloading ~p after ~p seconds.",
-            [N,Id,cycle_logic:seconds_elapsed(StartT)]),
+          LogF(info, "grib_server [~p] -> finished downloading ~p after ~p seconds.", [N,Id,cycle_logic:seconds_elapsed(StartT)]),
           success;
         Error ->
           LogF(info, "grib_server [~p] -> failed to download ~p after ~p seconds with error ~p",
-                [N,Id,cycle_logic:seconds_elapsed(StartT),Error]),
+            [N,Id,cycle_logic:seconds_elapsed(StartT),Error]),
           Id
       end end, List),
-  lists:filter(fun (X) -> not X == success end, Results).
+  lists:filter(fun (X) -> not (X == success) end, Results).
 
 
--spec find_serverside_missing_gribs([string()],#grib_source{}) -> [string()].
-find_serverside_missing_gribs(List,#grib_source{url_prefix=Pfix}) ->
-  lists:filter(fun(Id) ->
-        Url = Pfix ++ "/" ++ Id,
-        case httpc:request(head, {Url,[]},[],[]) of
-          {ok, {{_,200,_},_,_}} ->
-            false;
-          _ ->
-            true
-        end
-    end, List).
+-spec check_remote_grib(string(),#grib_source{}) -> [{available,string(),integer()}|{missing,string()}].
+check_remote_grib(Id,#grib_source{url_prefix=Pfix}) ->
+  Url = Pfix ++ "/" ++ Id,
+  case httpc:request(head, {Url,[]},[],[]) of
+      {ok, {{_,200,_},Hdrs,_}} ->
+        CL = proplists:get_value("content-length",Hdrs),
+        {present,Id,CL};
+      _ ->
+        {missing,Id}
+  end.
+
+
+-spec check_local_grib(string()) -> boolean.
+check_local_grib(Path) ->
+  case filelib:is_regular(Path) of
+    true ->
+      case file:read_file(Path ++ ".info") of
+        {ok,B} ->
+          Len = list_to_integer(binary_to_list(B)),
+          case filelib:file_size(Path) of
+            Len ->
+              false;
+            _WrongLen ->
+              file:delete(Path),
+              file:delete(Path ++ ".info"),
+              true
+          end;
+        _ ->
+          true
+      end;
+    false ->
+      true
+  end.
+
+
+-spec write_grib_info([{present,string(),pos_integer()}],string(),string()) -> ok.
+write_grib_info(Infos,StorDir,Domain) ->
+  filelib:ensure_dir(filename:join([StorDir,Domain,"fakefile"])),
+  lists:map(fun ({present,Id,Len}) ->
+        Path = filename:join([StorDir,Domain,Id ++ ".info"]),
+        filelib:ensure_dir(Path),
+        file:write_file(Path,Len) end, Infos),
+  ok.
 
 
 -spec retrieve_gribs(calendar:datetime(),calendar:datetime(),calendar:datetime(),integer(),string(),#grib_source{},fun()) ->
@@ -128,24 +170,32 @@ find_serverside_missing_gribs(List,#grib_source{url_prefix=Pfix}) ->
 retrieve_gribs(From,To,AtTime,Delta,StorDir,GS=#grib_source{name=N,domain=D},LogF) ->
   LogF(info, "grib_server [~p] -> received request from ~w to ~w at time ~w delta ~p", [N,From,To,AtTime,Delta]),
   case compute_manifest(From,To,AtTime,Delta,GS) of
-    {CovFrom,CovTo,Ids} ->
-      LogF(info,"grib_server [~p] -> manifest has ~p files covering interval from ~w to ~w", [N,length(Ids),CovFrom,CovTo]),
-      NonLocals = lists:filter(fun (X) -> not filelib:is_regular(filename:join([StorDir,D,X])) end, Ids),
-      case find_serverside_missing_gribs(NonLocals,GS) of
+    {Cycle,CovFrom,CovTo,Ids} ->
+      LogF(info,"grib_server [~p] -> manifest has ~p files covering interval from ~w to ~w from cycle ~w", [N,length(Ids),CovFrom,CovTo,Cycle]),
+      NonLocals = lists:filter(fun (X) -> check_local_grib(filename:join([StorDir,D,X])) end, Ids),
+      case NonLocals of
         [] ->
-          LogF(info, "grib_server [~p] -> ~p gribs available locally, ~p available remotely, downloading now",
-            [N,length(Ids)-length(NonLocals),length(NonLocals)]),
-          case download_gribs(NonLocals,StorDir,GS,LogF) of
+          LogF(info, "grib_server [~p] -> all ~p gribs available locally, returning immediately.", [N,length(Ids)]),
+          {CovFrom,CovTo,Ids};
+        _NotEmpty ->
+          RemoteInfo = lists:map(fun(X) -> check_remote_grib(X,GS) end,NonLocals),
+          case lists:filter(fun ({missing,_}) -> true; (_) -> false end, RemoteInfo) of
             [] ->
-              LogF(info, "grib_server [~p] -> all ~p files downloaded successfully, returning manifest covering [~w,~w] with ~p total files.",
-                    [N,length(NonLocals),CovFrom,CovTo,length(Ids)]),
-                  {CovFrom,CovTo,lists:map(fun (X) -> filename:join([StorDir,D,X]) end, Ids)};
-            Failures ->
-              {downloads_failed,Failures}
-          end;
-        MissingGribs ->
-          LogF(warn, "grib_server [~p] -> ~p gribs are not available locally or remotely, unsatisfiable request.", [N,length(MissingGribs)]),
-          {missing_gribs,MissingGribs}
+              LogF(info, "grib_server [~p] -> of ~p files, ~p checked & available locally, ~p available remotely, downloading now",
+                [N,length(Ids),length(Ids)-length(NonLocals),length(NonLocals)]),
+              write_grib_info(RemoteInfo,StorDir,D),
+              case download_gribs(NonLocals,StorDir,GS,LogF) of
+                [] ->
+                  LogF(info, "grib_server [~p] -> all ~p remote files downloaded successfully, returning manifest covering [~w,~w] with ~p total files.",
+                        [N,length(NonLocals),CovFrom,CovTo,length(Ids)]),
+                      {CovFrom,CovTo,lists:map(fun (X) -> filename:join([StorDir,D,X]) end, Ids)};
+                Failures ->
+                  {downloads_failed,Failures}
+              end;
+            MissingGribs ->
+              LogF(warn, "grib_server [~p] -> ~p gribs are not available locally or remotely, unsatisfiable request.", [N,length(MissingGribs)]),
+              {missing_gribs,lists:map(fun ({missing,X}) -> X end, MissingGribs)}
+          end
       end;
     unsatisfiable ->
       LogF(warn, "grib_server [~p] -> cannot satisfy request from ~w to ~w at time ~w with Delta ~w", [N,From,To,AtTime,Delta])
