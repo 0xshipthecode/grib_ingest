2c97b3cd43dfb10d22a08511d2836e266c647f86
Martin Vejmelka <vejmelkam@gmail.com>
2014-02-28 00:59:28 -0700

    added forgotten hrl file

---
+++ include/grib_ingest.hrl
@@ -0,0 +1,23 @@
+
+-record(grib_source,{
+    % name of the grib source
+    name :: atom()|string(),
+    % daily cycles recorded as hours since midnight, e.g. [0,6,15,18] in correct timezone
+    cycles :: [non_neg_integer()],
+    % delay between cycle time and forecast availability [hrs]
+    delay :: non_neg_integer(),
+    % the fixed part of the URL where the grib2 files are located
+    url_prefix :: string(),
+    % naming function, constructs the variable part of the URL and also functions as
+    % a relative path on the local filesystem
+    name_fun :: fun(),
+    % the vtable file that identifies the fields in the GRIB2 files from this source for WPS
+    domain :: string(),
+    % the hours [since cycle time] for which grib files are available every cycle
+    file_hours :: [non_neg_integer()],
+    % maximum concurrent downloads allowed for this source
+    max_downloads :: pos_integer(),
+    % extra entries that must be added to the WPS namelist to ensure correct function
+    grib_info :: [{any(),any()}]
+}).
+
